1d5520e94184359aefa1efa840960815d9f74983
Adel Ngo <adel.ngo@yahoo.com>
2023-01-22 21:37:49 +0000

    Added Exercises.

---
+++ 9_exercises/9.8_exercise.rb
@@ -0,0 +1,12 @@
+
+=begin
+
+Create a Hash, with one key-value pair, using both Ruby syntax styles.
+
+=end
+my_hash = {Batman: "Bruce Wayne"}
+my_hash = {:Batman => "Bruce Wayne"}
+
+#my_hash[:Superman] = "Clark Kent"
+
+p my_hash
